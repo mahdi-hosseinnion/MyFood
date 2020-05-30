186c7a1973eaea32c09a102ce4b2add47e20a966
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2020-05-30 13:41:26 +0330

    7-Retrofit Singleton

---
+++ app/src/main/java/com/example/myfood/requests/RecipeApi.java
@@ -0,0 +1,4 @@
+package com.example.myfood.requests;
+
+public interface RecipeApi {
+}
---
+++ app/src/main/java/com/example/myfood/requests/ServiceGenerator.java
@@ -0,0 +1,19 @@
+package com.example.myfood.requests;
+
+import com.example.myfood.util.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit retrofit = retrofitBuilder.build();
+    private static RecipeApi recipeApi = retrofit.create(RecipeApi.class);
+
+    public static RecipeApi getRecipeApi() {
+        return recipeApi;
+    }
+}
