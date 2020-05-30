8baaa310826b2f908295ed3d30c98cfb195d60a2
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2020-05-30 13:32:10 +0330

    6-Retrofit Dependencies

---
+++ app/src/main/java/com/example/myfood/util/Constants.java
@@ -0,0 +1,6 @@
+package com.example.myfood.util;
+
+public class Constants {
+    public static final String BASE_URL="https://recipesapi.herokuapp.com";
+    public static final String API_KEY="";
+}
