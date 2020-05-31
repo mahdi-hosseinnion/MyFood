2fce4a7304bbe2f23963a71eca428513846fb84c
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2020-05-31 13:14:34 +0330

    10-Retrofit Interface Method

--- app/src/main/java/com/example/myfood/requests/RecipeApi.java
+++ app/src/main/java/com/example/myfood/requests/RecipeApi.java
@@ -1,4 +1,23 @@
 package com.example.myfood.requests;
 
+import com.example.myfood.requests.responses.RecipeResponse;
+import com.example.myfood.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+    //searchRecipe Request
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("q") String query,
+            @Query("page")String page
+    );
+    //get Recipe Request
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("rId")String rId
+    );
+
 }
