8ff959e57fd9c2a324148bdfbb9babc7c1a6ac3b
mahdi-hosseinnion <45493154+mahdi-hosseinnion@users.noreply.github.com>
2020-06-02 10:02:05 +0330

    19-Executing a Runnable with and executor

--- app/src/main/java/com/example/myfood/requests/RecipeApiClient.java
+++ app/src/main/java/com/example/myfood/requests/RecipeApiClient.java
@@ -1,41 +1,49 @@
 package com.example.myfood.requests;
 
+import android.util.Log;
+
 import com.example.myfood.AppExecutors;
 import com.example.myfood.Models.Recipe;
+import com.example.myfood.requests.responses.RecipeSearchResponse;
 import com.example.myfood.util.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class RecipeApiClient {
+    private static final String TAG = "RecipeApiClient";
+    //var
     private static RecipeApiClient instance;
     private MutableLiveData<List<Recipe>> mRecipes;
-    public static RecipeApiClient getInstance(){
-        if (instance==null)
-            instance=new RecipeApiClient();
+    private RetrieveRecipeRunnable mRetrieveRecipeRunnable;
+
+    public static RecipeApiClient getInstance() {
+        if (instance == null)
+            instance = new RecipeApiClient();
         return instance;
     }
 
     private RecipeApiClient() {
-        mRecipes=new MutableLiveData<>();
+        mRecipes = new MutableLiveData<>();
     }
 
     public LiveData<List<Recipe>> getRecipes() {
         return mRecipes;
     }
-    private void searchRecipeApi(String query,int page){
 
-        final Future handler = AppExecutors.getInstance().getNetWordIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                //retrieve data form api
-            }
-        });
+    private void searchRecipeApi(String query, int page) {
+        if (mRetrieveRecipeRunnable!=null)
+            mRetrieveRecipeRunnable=null;
+        mRetrieveRecipeRunnable=new RetrieveRecipeRunnable(query,page);
+        final Future handler = AppExecutors.getInstance().getNetWordIO().submit(mRetrieveRecipeRunnable);
         AppExecutors.getInstance().getNetWordIO().schedule(new Runnable() {
             @Override
             public void run() {
@@ -45,5 +53,58 @@ public class RecipeApiClient {
         }, Constants.TIME_OUT, TimeUnit.MILLISECONDS);
     }
 
+    private class RetrieveRecipeRunnable implements Runnable {
+        private String query;
+        private int pageNO;
+        private boolean isCanceled;
+
+        public RetrieveRecipeRunnable(String query, int pageNO) {
+            this.query = query;
+            this.pageNO = pageNO;
+            isCanceled = false;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+
+                Response response = getRecipes(query, pageNO).execute();
+
+                if (isCanceled) {
+                    return;
+                }
+                if (response.code() == 200) {
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse) response.body()).getRecipes());
+                    if (pageNO == 1) {
+                        mRecipes.postValue(list);
+
+                    } else {
+                        List<Recipe> currentRecipes = mRecipes.getValue();
+                        currentRecipes.addAll(list);
+                        mRecipes.postValue(list);
+                    }
+                } else {
+                    Log.e(TAG, "run: " + response.errorBody());
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNO) {
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    query,
+                    String.valueOf(pageNO)
+            );
+        }
+
+        public void cancelRequest() {
+            isCanceled = true;
+        }
+    }
 
 }
