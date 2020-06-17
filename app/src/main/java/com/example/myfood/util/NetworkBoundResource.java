fc273f6d00dc15f3e44776baa447cda961b95774
seyed mahdi <ssmmhh1382@gmail.com>
2020-06-17 17:24:07 +0100

    8-Network bound Resource Part 1/3

---
+++ app/src/main/java/com/example/myfood/util/NetworkBoundResource.java
@@ -0,0 +1,48 @@
+package com.example.myfood.util;
+
+import android.util.Log;
+
+import com.example.myfood.AppExecutors;
+import com.example.myfood.requests.responses.ApiResponse;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
+
+// CacheObject: Type for the Resource data. (database cache)
+// RequestObject: Type for the API response. (network request)
+public abstract class NetworkBoundResource<CacheObject, RequestObject> {
+
+    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+
+    // Called to save the result of the API response into the database.
+    @WorkerThread
+    protected abstract void saveCallResult(@NonNull RequestObject item);
+
+    // Called with the data in the database to decide whether to fetch
+    // potentially updated data from the network.
+    @MainThread
+    protected abstract boolean shouldFetch(@Nullable CacheObject data);
+
+    // Called to get the cached data from the database.
+    @NonNull
+    @MainThread
+    protected abstract LiveData<CacheObject> loadFromDb();
+
+    // Called to create the API call.
+    @NonNull
+    @MainThread
+    protected abstract LiveData<ApiResponse<RequestObject>> createCall();
+
+    // Returns a LiveData object that represents the resource that's implemented
+    // in the base class.
+    public final LiveData<Resource<CacheObject>> getAsLiveData() {
+        return results;
+    }
+
+    ;
+}
