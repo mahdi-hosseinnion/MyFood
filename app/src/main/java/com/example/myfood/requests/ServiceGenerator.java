c0ff429f03cade10223100f80aef27af69f146a0
seyed mahdi <ssmmhh1382@gmail.com>
2020-06-29 09:34:05 +0100

    27-Okhttp network request.

--- app/src/main/java/com/example/myfood/requests/ServiceGenerator.java
+++ app/src/main/java/com/example/myfood/requests/ServiceGenerator.java
@@ -1,16 +1,35 @@
 package com.example.myfood.requests;
 
+import android.os.ParcelUuid;
+
 import com.example.myfood.util.Constants;
 import com.example.myfood.util.LiveDataCallAdapter;
 import com.example.myfood.util.LiveDataCallAdapterFactory;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.example.myfood.util.Constants.CONNECTION_TIMEOUT;
+import static com.example.myfood.util.Constants.READ_TIMEOUT;
+import static com.example.myfood.util.Constants.WRITE_TIMEOUT;
+
 public class ServiceGenerator {
+    private static OkHttpClient client = new OkHttpClient().newBuilder()
+            //establish connection to server
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            //time between each byte read from server
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            //time between each byte write from server
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(false)
+            .build();
     private static Retrofit.Builder retrofitBuilder =
             new Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
+                    .client(client)
                     .addCallAdapterFactory(new LiveDataCallAdapterFactory())
                     .addConverterFactory(GsonConverterFactory.create());
     private static Retrofit retrofit = retrofitBuilder.build();
--- app/src/main/java/com/example/myfood/util/Constants.java
+++ app/src/main/java/com/example/myfood/util/Constants.java
@@ -6,7 +6,9 @@ public class Constants {
     public static final int TIME_OUT = 5000;
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
-
+    public static final int CONNECTION_TIMEOUT=10;//10 SECOND
+    public static final int READ_TIMEOUT=2;//1 SECOND
+    public static final int WRITE_TIMEOUT=2;//1 SECOND
     public static final String[] DEFAULT_SEARCH_CATEGORY_IMAGES =
             {
                     "barbeque",
